c3f3eb45fb43e9055c95452183a36a0588e39bd3
Jávorszky Mazsi <javorszky.mariann@gmail.com>
2017-01-09 18:50:48 +0100

    read_file()

---
+++ readfile.cpp
@@ -0,0 +1,12 @@
+//read file into string by line
+
+std::string read_file() {
+  std::ifstream source_file("file.txt");
+  std::string buffer;
+  std::string content = "";
+  while (getline(source_file, buffer)) {
+    content += buffer + "\n";
+  }
+  source_file.close();
+  return line;
+}
